359b7c78fb606f1b87da3f9935330252c223f5ef
Vicente <vicente.sepulveda.g@usach.cl>
2022-07-05 19:49:43 -0400

    ll

--- index.js
+++ index.js
@@ -12,39 +12,46 @@ const client = new hive.HiveClient(
 const utils = new hive.HiveUtils(
     TCLIService_types
 );
- 
-client.connect(
-    {
-        host: 'localhost',
-        port: 25000
-    },
-    new hive.connections.TcpConnection(),
-    new hive.auth.PlainTcpAuthentication()
-    
-).then(async client => {
 
-    const session = await client.openSession({
-        client_protocol: TCLIService_types.TProtocolVersion.HIVE_CLI_SERVICE_PROTOCOL_V10
+async function hola() {
+
+    return client.connect(
+        {
+            host: 'localhost',
+            port: 25000
+        },
+        new hive.connections.TcpConnection(),
+        new hive.auth.PlainTcpAuthentication()
+        
+    ).then(async client => {
+
+        const session = await client.openSession({
+            client_protocol: TCLIService_types.TProtocolVersion.HIVE_CLI_SERVICE_PROTOCOL_V10
+        });
+        const selectDataOperation = await session.executeStatement(
+            'SELECT AVG(VALOR) FROM BITCOIN '
+        );
+        await utils.waitUntilReady(selectDataOperation, false, () => {});
+        await utils.fetchAll(selectDataOperation);
+        await selectDataOperation.close();
+        
+        
+        
+        await session.close();
+        console.log(utils.getResult(selectDataOperation).getValue());
+        
+        return utils.getResult(selectDataOperation).getValue(); 
+
+    }).catch(error => {
+        console.log(error);
     });
-    const selectDataOperation = await session.executeStatement(
-        'SELECT AVG(VALOR) FROM BITCOIN '
-    );
-    await utils.waitUntilReady(selectDataOperation, false, () => {});
-    await utils.fetchAll(selectDataOperation);
-    await selectDataOperation.close();
-    
-    console.log(utils.getResult(selectDataOperation).getValue());
-    
-    await session.close();
 
-    
-}).catch(error => {
-    console.log(error);
-});
+}
 
-app.get('/prom', (req,res) => {
-    const result = client.connect();
-    console.log("hola:",result);
+app.get('/prom', async (req,res) => {
+/*     const result =  client.connect();
+    console.log("hola:",result); */
+    return res.status(200) .json( await hola());
  })
 app.listen(PORT, () => {
     console.log("Servidor corriendo en el puerto", PORT )
